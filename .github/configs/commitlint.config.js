555f91c7914eaa4f7efdbc1d0e4189324bc978e4
Stephan Kromberg <s.kromberg@clevershuttle.de>
2022-03-23 23:36:52 +0100

    feat: Upgrade to latest composite-actions; Optimize workflow
    Refs: OMG-2295, OMG-2646

--- .github/configs/commitlint.config.js
+++ .github/configs/commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
-  extends: ["@commitlint/config-conventional"],
+  // see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-angular
+  extends: ["@commitlint/config-angular"],
   rules: {
     "scope-case": [1, "always", "upper-case"],
     "subject-case": [1, "always", "sentence-case"],
