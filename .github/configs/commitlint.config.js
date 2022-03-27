5476bfe205aa938b01cf4e5139696bd2b5d0c08d
Stephan Kromberg <s.kromberg@clevershuttle.de>
2022-03-27 23:10:29 +0200

    feat: Use config-conventional for lint commits
    Refs: OMG-2650, OMG-2651

--- .github/configs/commitlint.config.js
+++ .github/configs/commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-angular
-  extends: ["@commitlint/config-angular"],
+  // see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-case": [1, "always", "upper-case"],
     "subject-case": [1, "always", "sentence-case"],
